c522ff22d92a4a2052c1de05252c622a9893be62
devashish1606 <devashish.cpj2013@gmail.comm>
2021-07-19 09:58:50 +0530

    Bounce Animation App

---
+++ app/src/main/java/com/example/bounceanimation/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.bounceanimation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+    Button button;
+    ImageView imageView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button = findViewById(R.id.button);
+        imageView = findViewById(R.id.imageView);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Animation animation = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.bounce);
+                imageView.startAnimation(animation);
+            }
+        });
+    }
+}
